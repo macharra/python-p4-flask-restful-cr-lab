d6240a66d18e9ae03c735be51fc04a2fe95fa89b
rogers.machara <rogersmachara13@gmail.com>
2024-07-14 18:20:14 +0300

    rest

--- server/models.py
+++ server/models.py
@@ -1,3 +1,4 @@
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 
@@ -7,3 +8,9 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(db.Float)
+
+    def __repr__(self):
+        return f'<Plant {self.name} | Price: {self.price}>'
